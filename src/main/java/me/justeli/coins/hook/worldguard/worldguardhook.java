ec3663905d3868046c4cb4ad41fa7d4bf5751530
wolfebersahd <j_wolfeinc@hotmail.com>
2025-12-29 10:49:46 -0500

    Add WorldGuardHook for coin drop flag management

---
+++ src/main/java/me/justeli/coins/hook/worldguard/worldguardhook.java
@@ -0,0 +1,45 @@
+package me.justeli.coins.hooks;
+
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import org.bukkit.Location;
+
+public final class WorldGuardHook {
+
+    public static StateFlag COINS_DROP_FLAG;
+
+    private WorldGuardHook() {}
+
+    public static void register() {
+        try {
+            StateFlag flag = new StateFlag("coins-drop", true);
+            WorldGuard.getInstance().getFlagRegistry().register(flag);
+            COINS_DROP_FLAG = flag;
+        } catch (FlagConflictException e) {
+            Flag<?> existing = WorldGuard.getInstance().getFlagRegistry().get("coins-drop");
+            if (existing instanceof StateFlag) {
+                COINS_DROP_FLAG = (StateFlag) existing;
+            }
+        }
+    }
+
+    public static boolean canDropCoins(Location location) {
+        if (COINS_DROP_FLAG == null) {
+            return true;
+        }
+
+        RegionContainer container = WorldGuard.getInstance()
+                .getPlatform()
+                .getRegionContainer();
+
+        return container.createQuery().testState(
+                BukkitAdapter.adapt(location),
+                null,
+                COINS_DROP_FLAG
+        );
+    }
+}
