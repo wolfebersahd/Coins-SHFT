cd5dd1c570e74be7f750d18789e5eebdc5ddb7a6
wolfebersahd <j_wolfeinc@hotmail.com>
2025-12-29 13:10:02 -0500

    Update event listener to use PlayerPickupItemEvent

--- src/main/java/me/justeli/coins/handler/listener/PaperEventListener.java
+++ src/main/java/me/justeli/coins/handler/listener/PaperEventListener.java
@@ -4,21 +4,20 @@ import me.justeli.coins.Coins;
 import me.justeli.coins.event.PickupEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent; // <- changed
 
 /* Eli @ September 13, 2020 (creation) */
-public final class PaperEventListener
-    implements Listener
+public final class PaperEventListener implements Listener
 {
     private final Coins coins;
 
-    public PaperEventListener (Coins coins)
+    public PaperEventListener(Coins coins)
     {
         this.coins = coins;
     }
 
     @EventHandler
-    public void onPickupPaper (PlayerAttemptPickupItemEvent event)
+    public void onPickupPaper(PlayerPickupItemEvent event) // <- changed
     {
         PickupEvent registerEvent = new PickupEvent(event.getPlayer(), event.getItem());
         this.coins.getServer().getPluginManager().callEvent(registerEvent);
