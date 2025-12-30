76cb9c2d787ef9e664ac2152dcc4023560609b0b
wolfebersahd <j_wolfeinc@hotmail.com>
2025-12-30 14:53:56 -0500

    Update WorldGuardHook.java

--- src/main/java/me/justeli/coins/hook/worldguard/WorldGuardHook.java
+++ src/main/java/me/justeli/coins/hook/worldguard/WorldGuardHook.java
@@ -2,8 +2,8 @@ package me.justeli.coins.hooks;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
+import me.justeli.coins.CustomFlags;
 import org.bukkit.Location;
 
 public final class WorldGuardHook {
@@ -14,9 +14,8 @@ public final class WorldGuardHook {
      * @return false if coins-drop is DENY in any applicable region
      */
     public static boolean canDropCoins(Location location) {
-        // If flag was not registered for some reason, allow drops
         if (CustomFlags.COINS_DROP_FLAG == null) {
-            return true;
+            return true; // fail open
         }
 
         RegionQuery query = WorldGuard.getInstance()
@@ -24,7 +23,6 @@ public final class WorldGuardHook {
                 .getRegionContainer()
                 .createQuery();
 
-        // testState returns TRUE only if the final result is ALLOW
         return query.testState(
                 BukkitAdapter.adapt(location),
                 null,
